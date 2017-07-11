8685b1598a1ebcf4432dc81154f86a53b76e2be5
Chris Nyaga <nyagachris75@gmail.com>
2017-07-11 21:08:11 +0300

    Class method .all defined to retrieve all doctors from the database and push them into an arraylist which is finally returned.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,4 +6,17 @@ class Doctor
   @speciality = attributes.fetch(:speciality)
   @id = attributes.fetch(:id)
   end
+
+  define_singleton_method(:all) do
+    result = DB.exec('SELECT * FROM doctors;')
+    doctors = []
+    result.each() do |doctor|
+      name = doctor.fetch('name')
+      speciality = doctor.fetch('speciality')
+      id = doctor.fetch('id').to_i
+      new_doctor = Doctor.new(id:id,name:name, speciality:speciality)
+      doctors.push(new_doctor)
+    end
+    doctors
+  end
 end
