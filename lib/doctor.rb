316ea9f0121800836cfc68b8a39e76877da7ae8f
Chris Nyaga <nyagachris75@gmail.com>
2017-07-11 21:07:10 +0300

    Attribute reader defined to generate automatic getters and initialize method defined with required parameters.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,2 +1,9 @@
 class Doctor
+  attr_reader(:id, :name, :speciality)
+
+  define_method(:initialize) do |attributes|
+  @name = attributes.fetch(:name)
+  @speciality = attributes.fetch(:speciality)
+  @id = attributes.fetch(:id)
+  end
 end
